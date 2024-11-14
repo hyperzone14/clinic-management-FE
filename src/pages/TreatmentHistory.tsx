753d8c92eb3db6f248eb75d28e48d8e3592ca6af
Vo Dang Trinh <109226619+hyperzone14@users.noreply.github.com>
2024-11-15 00:12:35 +0700

    update page treatment-history - 11/15/2024

---
+++ src/pages/TreatmentHistory.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TreatmentHistory = () => {
+    return (
+        <>
+            <div>this is treatment history</div>
+        </>
+    )
+}
+
+export default TreatmentHistory
