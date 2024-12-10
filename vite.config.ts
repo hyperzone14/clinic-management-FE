8541763bad4c5eea2e2b71b0b8110b2c0d0a9088
Nguyễn Văn Anh Đồng <126852621+memo257@users.noreply.github.com>
2024-12-10 23:21:12 +0700

    test vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Add this line for proper routing
   server: {
     proxy: {
       "/api": {
-        target: "https://clinic-management-vdb.onrender.com", // Backend server
+        target: "https://clinic-management-vdb.onrender.com",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Optional: Removes '/api' prefix
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
