337d01f2c5341cc661e3f37c469ca46f12ae42cd
Vo Dang Trinh <109226619+hyperzone14@users.noreply.github.com>
2025-05-03 15:30:40 +0700

    update

--- src/utils/security/config/SecurityConfig.ts
+++ src/utils/security/config/SecurityConfig.ts
@@ -1,7 +1,8 @@
 // src/utils/security/config/SecurityConfig.ts
 
 export const SecurityConfig = {
-  API_URL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+  // API_URL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+  API_URL: import.meta.env.VITE_API_URL,
   TOKEN_KEY: "auth_token",
   AUTH_HEADER: "Authorization",
   AUTH_PREFIX: "Bearer",
