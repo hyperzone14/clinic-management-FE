ffc11f7f011199a324975d87433a69bdf557b3c9
Vo Dang Trinh <109226619+hyperzone14@users.noreply.github.com>
2025-05-03 21:08:53 +0700

    update hosting

--- src/utils/security/config/SecurityConfig.ts
+++ src/utils/security/config/SecurityConfig.ts
@@ -2,7 +2,9 @@
 
 export const SecurityConfig = {
   // API_URL: import.meta.env.VITE_API_URL || "http://localhost:8080",
-  API_URL: import.meta.env.VITE_API_URL,
+  API_URL:
+    import.meta.env.VITE_API_URL ||
+    "https://clinic-management-tdd-ee9f27f356d8.herokuapp.com",
   TOKEN_KEY: "auth_token",
   AUTH_HEADER: "Authorization",
   AUTH_PREFIX: "Bearer",
