60a9f26179073abf3ab15f6622ce5d54fed6007d
Nguyễn Văn Anh Đồng <126852621+memo257@users.noreply.github.com>
2025-05-06 20:12:29 +0700

    update chat bot

--- src/utils/websocket-config.ts
+++ src/utils/websocket-config.ts
@@ -5,7 +5,7 @@ import SockJS from "sockjs-client";
 export const createStompClient = (): CompatClient => {
   // Add full URL including port
   const socket = new SockJS(
-    "https://clinic-management-tdd-ee9f27f356d8.herokuapp.com/api/websocket"
+    "https://clinic-management-tdd-ee9f27f356d8.herokuapp.com/websocket"
   ); //http://localhost:8080/websocket
   const stompClient = Stomp.over(socket);
 
